e539a7073f6d26133a6633b26e2e47ca5b0aff43
Álefe Alves <alefealves@hotmail.com>
2024-05-07 10:57:44 -0400

    feat: add createPato

--- src/main/java/alefe/alves/apiduck/controllers/PatoController.java
+++ src/main/java/alefe/alves/apiduck/controllers/PatoController.java
@@ -1,11 +1,16 @@
 package alefe.alves.apiduck.controllers;
 
 import alefe.alves.apiduck.dtos.PatoDTO;
+import alefe.alves.apiduck.dtos.ResponsePato;
 import alefe.alves.apiduck.services.PatoService;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pato")
@@ -20,4 +25,16 @@ public class PatoController {
         return new ResponseEntity<>(pato, HttpStatus.OK);
     }
 
+    @GetMapping
+    public ResponseEntity<List<ResponsePato>> getAllPatos() throws Exception{
+        List<ResponsePato> patos = this.patoService.getAllPatos();
+        return new ResponseEntity<>(patos, HttpStatus.OK);
+    }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<PatoDTO> createPato(@RequestBody @Valid PatoDTO dto) throws Exception {
+        PatoDTO pato = patoService.createPato(dto);
+        return new ResponseEntity<>(pato, HttpStatus.CREATED);
+    }
 }
--- src/main/java/alefe/alves/apiduck/services/PatoService.java
+++ src/main/java/alefe/alves/apiduck/services/PatoService.java
@@ -3,14 +3,18 @@ package alefe.alves.apiduck.services;
 import alefe.alves.apiduck.dtos.PatoDTO;
 import alefe.alves.apiduck.dtos.PatoUpdateDTO;
 import alefe.alves.apiduck.dtos.ResponsePato;
+import alefe.alves.apiduck.enums.TipoPato;
 import alefe.alves.apiduck.exceptions.PatoNotFoundException;
 import alefe.alves.apiduck.interfaces.PatoInterface;
 import alefe.alves.apiduck.models.pato.Pato;
 import alefe.alves.apiduck.repositories.PatoRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,7 +33,9 @@ public class PatoService implements PatoInterface {
         Optional<Pato> optPato = this.repository.findPatoById(id);
         if(optPato.isPresent()){
             Pato pato = optPato.get();
-            return modelMapper.map(pato, PatoDTO.class);
+            PatoDTO patoDTO = modelMapper.map(pato, PatoDTO.class);
+            patoDTO.setMae_id(pato.getMae().getId());
+            return patoDTO;
         } else {
             throw new PatoNotFoundException("Pato de id "+id+" não foi encontrado.");
         }
@@ -37,12 +43,49 @@ public class PatoService implements PatoInterface {
 
     @Override
     public List<ResponsePato> getAllPatos() throws Exception {
-        return List.of();
+        try {
+
+            List<ResponsePato> responsePatos = new ArrayList<>();
+            Sort sort = Sort.by(Sort.Direction.ASC, "id");
+            List<Pato> patoList = this.repository.findAll(sort);
+
+            for(Pato patoId : patoList){
+                Optional<Pato> OptPato = this.repository.findPatoById(patoId.getId());
+                if(OptPato.isPresent()) {
+                    Pato pato = OptPato.get();
+                    ResponsePato responsePato = createResponse(pato);
+                    responsePatos.add(responsePato);
+                }
+            }
+
+            return responsePatos;
+        } catch (Exception e) {
+            throw new Exception("Ocorreu um erro ao buscar os patos.");
+        }
     }
 
     @Override
     public PatoDTO createPato(PatoDTO dto) throws Exception {
-        return null;
+        if (dto.getTipo() == TipoPato.FILHO) {
+            // Verifica se foi informado mae_id
+            if (dto.getMae_id() != null) {
+                // Busca o pato mãe pelo ID
+                Pato mae = repository.findPatoById(dto.getMae_id())
+                        .orElseThrow(() -> new Exception("ID da pata_mae inválido"));
+                // O valor do pato que foi vinculado como mae fica em R$50
+                mae.setValor(BigDecimal.valueOf(50.00));
+                dto.setMae(mae);
+                dto.setTipo(TipoPato.FILHO);
+            } else {
+                throw new Exception("Informe o id da Pata Mae");
+            }
+        }
+
+        Pato newPato = new Pato(dto);
+        this.repository.save(newPato);
+        PatoDTO patoDTO = modelMapper.map(newPato, PatoDTO.class);
+        patoDTO.setMae_id(dto.getMae_id());
+        return patoDTO;
     }
 
     @Override
@@ -53,5 +96,20 @@ public class PatoService implements PatoInterface {
     @Override
     public void deletePato(Long id) throws Exception {
 
+    }
+
+    public ResponsePato createResponse(Pato pato) throws Exception {
+        ResponsePato responsePato = new ResponsePato();
+
+        responsePato.setId(pato.getId());
+        responsePato.setTipoPato(pato.getTipo());
+        responsePato.setValor(pato.getValor());
+        responsePato.setStatus(pato.getStatus());
+        if (pato.getMae() != null)
+            responsePato.setMae_id(pato.getMae().getId());
+        /*if (pato.getVenda() != null)
+            responsePato.setVenda_id(pato.getVenda().getId());*/
+
+        return responsePato;
     }
 }
